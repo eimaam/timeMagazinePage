0387d242a887367c174e017dcdf895b15747194d
eimaam <imamddahir@gmail.com>
2022-03-01 10:20:45 +0100

    added functionality to harmburger menu

---
+++ script.js
@@ -0,0 +1,10 @@
+function showHide(){
+    var harmburger = document.getElementById("nav");
+    if(harmburger.style.visibility !== "hidden"){
+        harmburger.style.visibility = "hidden";
+    }
+        else{
+            harmburger.style.visibility = "visible";
+        }
+
+    }
